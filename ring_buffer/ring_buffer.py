b5c533d075d76db085ded02def245aa39fb8d8cd
clem9281 <clem9281@gmail.com>
2019-10-18 10:33:53 -0700

    fixes ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -3,13 +3,12 @@ class RingBuffer:
     self.capacity = capacity
     self.current = 0
     self.storage = [None]*capacity
-    self.oldest_index = 0
     
-  def iterate_oldest_index(self):
-    if self.oldest_index == self.capacity - 1:
-      self.oldest_index = 0
+  def iterate_current(self):
+    if self.current == self.capacity - 1:
+      self.current = 0
     else:
-      self.oldest_index += 1
+      self.current += 1
       
   def append(self, item):
     # if there are Nones in our storage we have not yet reached capacity
@@ -20,8 +19,8 @@ class RingBuffer:
           self.storage[i] = item
           break
     else:
-       self.storage[self.oldest_index] = item
-       self.iterate_oldest_index() 
+       self.storage[self.current] = item
+       self.iterate_current() 
 
   def get(self):
     if None in self.storage:
@@ -54,3 +53,31 @@ buffer.append('h')
 buffer.append('i')
 print(len(buffer.storage), 5)
 print(buffer.get(), ['f', 'g', 'h', 'i', 'e'])
+
+# *******************************************************************************
+# Ignore this probably, I was trying to do this with a Queue but I think it's probably
+# just better off with an array, an array preserves the order better
+# *******************************************************************************
+# from dll_queue import Queue
+# class RingBuffer:
+#   def __init__(self, capacity):
+#     self.capacity = capacity
+#     self.current = 0
+#     self.storage = Queue()
+#   def __str__(self):
+#     current_node = self.storage.storage.head
+#     arr = []
+#     while current_node:
+#       arr.append(current_node.value)
+#       current_node = current_node.next
+#     return f"{arr}"
+      
+#   def append(self, item):
+#     # print((self.storage.len()))
+#     if len(self.storage) >= self.capacity:
+#       self.storage.dequeue()
+#     self.storage.enqueue(item)
+
+#   def get(self):
+#     return self.storage
+# 
